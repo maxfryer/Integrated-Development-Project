726d49ef5748b2413aa07170350dbd9663395f4f
ShariceLiu <73257545+ShariceLiu@users.noreply.github.com>
2020-11-09 13:27:12 +0000

    Add files via upload

---
+++ idp test/read_data_pinA0/read_data_pinA0.ino
@@ -0,0 +1,17 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);           //Start serial and set the correct Baud Rate
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  
+  
+}
---
+++ idp test/servo_test/servo_test.ino
@@ -0,0 +1,27 @@
+// Include the Servo library 
+#include <Servo.h> 
+// Declare the Servo pin, 9 for servo2 and 10 for servo1
+int servoPin = 10; 
+
+int pos =0;  // variable to store the servo position
+
+// Create a servo object 
+Servo Servo1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Servo1.attach(servoPin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Servo1.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    Servo1.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
